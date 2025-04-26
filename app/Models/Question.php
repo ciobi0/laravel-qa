b6672c134860d4c982612c1d521c800064ac7a00
Cosmin Ciobanu <ciobanu.cosmin@yahoo.com>
2025-04-26 10:43:01 +0300

    prepopulate user and question tables using factories and seed

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,17 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
 
 class Question extends Model
 {
+    use HasFactory, Notifiable;
     protected $fillable = ['title', 'body'];
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function setTitleAttribute($value) {
+    public function setTitleAttribute($value)
+    {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Question;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(3)->create()->each(function ($user) {
+            $user
+                ->questions()
+                ->saveMany(Question::factory(rand(1, 5))->make());
+        });
     }
 }
