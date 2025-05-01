713bb9ea4135f777a0c9d2a8eb26d4f4d23cc138
Cosmin Ciobanu <ciobanu.cosmin@yahoo.com>
2025-05-01 10:13:54 +0300

    build question form

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -30,7 +30,7 @@ class Question extends Model
         return $this->created_at->diffForHumans();
     }
 
-    public function getStautsAttribute()
+    public function getStatusAttribute()
     {
         if ($this->answers > 0) {
             if ($this->best_answer_id) {
