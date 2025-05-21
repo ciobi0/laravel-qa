e11a4704d13984c2f08b9cca9ba83222da6ce111
Cosmin Ciobanu <ciobanu.cosmin@yahoo.com>
2025-05-21 21:57:23 +0300

    add vote controls to question and answers

---
+++ resources/fontawesome.js
@@ -0,0 +1,10 @@
+import fontawesome from '@fortawesome/fontawesome-free/js/all.js';
+import faCaretUp from '@fortawesome/fontawesome-free/svgs/solid/caret-up.svg';
+import faCaretDown from '@fortawesome/fontawesome-free/svgs/solid/caret-down.svg';
+import faStar from '@fortawesome/fontawesome-free/svgs/solid/star.svg';
+import faCheck from '@fortawesome/fontawesome-free/svgs/solid/check.svg';
+
+fontawesome.library.add(faCaretUp);
+fontawesome.library.add(faCaretDown);
+fontawesome.library.add(faStar);
+fontawesome.library.add(faCheck);
